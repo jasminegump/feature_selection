688564a4f71d66789f3ee272abf076ce05cb6c17
hyun1e <jasmine.kim.18@gmail.com>
2018-03-19 21:21:18 -0700

    All three search algorithms working. Needs clean up.

--- feature_selection.py
+++ feature_selection.py
@@ -1,7 +1,6 @@
 import re
 import pandas as pd
 import math
-import matplotlib.pyplot as plt
 import numpy as np
 import copy
 import sys
@@ -195,6 +194,7 @@ def forward_selection(data, num_features):
 			best_total_accuracy = best_so_far_accuracy
 		print("\n")
 	print("Finished search!!! The best feature subset is ", best_feature, "which has an accuracy of", best_total_accuracy*100, "%")
+	return best_feature, best_total_accuracy
 
 def backwards_selection(data, num_features):
 
@@ -226,15 +226,13 @@ def backwards_selection(data, num_features):
 					feature_to_remove = j
 				#feature_to_add_at_this_level.append(j)
 		if accuracy >= best_so_far_accuracy and (j > 1):
-			if found_all == 0:
-				print("Warning, accuracy has decreased! Continuing search in case of local maxima")
-			found_all = 1
+			print("Warning, accuracy has decreased! Continuing search in case of local maxima")
 
 		current_set_of_features.remove(feature_to_remove)
 		print("Features set", current_set_of_features, "was best, accuracy is", best_so_far_accuracy*100, "%")
 
 
-		if not found_all:
+		if best_so_far_accuracy >= best_total_accuracy:
 			best_feature = copy.deepcopy(current_set_of_features)
 			best_total_accuracy = best_so_far_accuracy
 
@@ -244,83 +242,82 @@ def backwards_selection(data, num_features):
 
 def jasmine_search_algorithm(data, num_features):
 
+	features = []
 	total_results = []
 	total_runs = 3
 	run_diff = 0.02
 	orig_data = copy.deepcopy(data)
+	untouched_original = copy.deepcopy(data)
+	for k in range(0,total_runs):
+		for l in range(0,total_runs):
+			data = orig_data
+			#delete random 5% of data
+			# resample 5%
+			resample = int(len(data)/100*40)
+			#print(len(data))
+			for m in range(0,resample):
+				temp_rand = random.randint(0,len(data)-1)
+				#print(temp_rand)
+				#https://chrisalbon.com/python/data_wrangling/pandas_dropping_column_and_rows/
+				data = data.drop(data.index[temp_rand])
+			#print(len(data))
 
-	for l in range(0,total_runs):
-		data = orig_data
+			best_feature, best_total_accuracy = forward_selection(data, num_features)
+			total_results.append(best_feature)
+		
+		strong_feature = find_strong_feature(total_results)
+		
+		s_feature = col_names[strong_feature]
+		s_feature = s_feature[1:]
+		features.append(int(s_feature))
+		print("")
+		print("Strongest Feature is", s_feature, ", hence removing it...")
+		print("")
+		#https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.drop.html
+		temp = col_names[strong_feature]
+		data = data.drop([temp], axis=1)
+		orig_data = orig_data.drop([temp], axis=1)
+		#print(data)
+		col_names.remove(temp)
+		num_features -= 1
+	print("FEATURES:", features)
 
-		print("LOOP:", l)
-
-		#delete random 5% of data
-		# resample 5%
-		resample = int(len(data)/100*5)
-		#print(len(data))
-		for m in range(0,resample):
-			temp_rand = random.randint(0,len(data)-1)
-			print(temp_rand)
-			#https://chrisalbon.com/python/data_wrangling/pandas_dropping_column_and_rows/
-			data = data.drop(data.index[temp_rand])
-		#print(len(data))
-
-		current_set_of_features = []
-		best_feature = []
-		best_total_accuracy = 0
-
-		# loop through the features
-
-		for i in range(1,num_features+1):
-			print ("On level ",i, " of the search tree")
-			best_so_far_accuracy = 0
-			feature_to_add_at_this_level = []
-			for j in range(1,num_features+1):
-				if j not in current_set_of_features:
-					print ("Considering adding the ",j, " feature")
-					accuracy = leave_one_out_cross_validation(data, current_set_of_features,j) # RANDOM input for now cause it don't matter
-					print("accuracy", accuracy, "best_so_far_accuracy", best_so_far_accuracy)
-					if (accuracy >= best_so_far_accuracy):
-						print("accuracy > best_so_far_accuracy")
-						best_so_far_accuracy = accuracy
-						if feature_to_add_at_this_level:
-							feature_to_add_at_this_level = []
-						feature_to_add_at_this_level.append(j)
-			current_set_of_features += feature_to_add_at_this_level
-			if (best_so_far_accuracy > best_total_accuracy) and (len(current_set_of_features) <= 3):
-				#print("!!!!!!")
-				best_feature = copy.deepcopy(current_set_of_features)
-				best_total_accuracy = best_so_far_accuracy
-			print("On level", i," I added feature ", feature_to_add_at_this_level,"to current set")
-			print("current_set_of_features:", current_set_of_features)
-			print("%%%%%%%%%%%%%%%%%%%%%%%%%")
-		print("FINISHED", best_feature, best_total_accuracy)
-
-		total_results.append(best_feature)
-	#print()	
-	print("FINISHED", total_results)
-
+	temp_final = []
+	final_a = []
+	for n in range(0,total_runs):
+		temp_final.append(features[n])
+		best_total_accuracy = training(untouched_original,temp_final,0)
+		final_a.append(best_total_accuracy)
+		print("temp_final",temp_final, "accuracy",best_total_accuracy)
+	max_accuracy = max(final_a)
+	final_set = features[:final_a.index(max_accuracy)+1]
+	print("Finished search!!! The best feature subset is ", final_set, "which has an accuracy of", max_accuracy*100, "%")
 	return total_results
+
+
+
+	#best_total_accuracy = training(untouched_original,features,0)
+	#print("Finished search!!! The best feature subset is ", features, "which has an accuracy of", best_total_accuracy*100, "%")
+	#return total_results
 
 def find_strong_feature(feature_list):
 	count = 0
 	strong_features = dict()
-	features_output = []
+	#features_output = []
 	for i in range(len(feature_list)):
 		for j in range(len(feature_list[i])):
 			if feature_list[i][j] in strong_features:
 				strong_features[feature_list[i][j]] = strong_features[feature_list[i][j]] + 1
 			else:
 				strong_features[feature_list[i][j]] = 1
-	print (strong_features)
+	#print (strong_features)
 	
 	#https://stackoverflow.com/questions/26871866/print-highest-value-in-dict-with-key
 	max_key = max(strong_features, key=strong_features.get)  # Just use 'min' instead of 'max' for minimum.
-	features_output.append(max_key)
+	features_output = max_key
 	max_key = strong_features.pop(max_key)
-	#print(max_key, strong_features[max_key])
-	max_key = max(strong_features, key=strong_features.get)  # Just use 'min' instead of 'max' for minimum.
-	features_output.append(max_key)
+	#max_key = max(strong_features, key=strong_features.get)  # Just use 'min' instead of 'max' for minimum.
+	#features_output.append(max_key)
 
 	return features_output
 
@@ -359,6 +356,9 @@ def main():
 
 	find_default(df, features_total)
 
+	temp = training(df, [6,2,3], 0)
+	print("ACCURACY",temp)
+
 	accuracy_percentage = accuracy*100
 	print("Running nearest neighbor with all ", features_total-1, "features, using 'leaving-one-out' evaluation, I get an accuracy of ", accuracy_percentage, "%")
 
@@ -376,43 +376,5 @@ def main():
 	end = time.time()
 	print("Time lapsed (seconds): ", end-start)
 
-
-	#print(features_total)
-	#print(df)
-
-	#print(features_total)
-	#print(df)
-
-	#training(df,[5,6,8],10)
-
-	#forward_selection(df, len(col_names)-1)
-	#backwards_selection(df, len(col_names)-1)
-
-
-	'''
-	results = third_algorithm(df, len(col_names)-1)
-	
-	strong_features = find_strong_feature(results)
-	#features = features + strong_features
-	print("strong_features", strong_features)
-	for i in strong_features:
-		#https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.drop.html
-		temp = col_names[i]
-		df = df.drop([temp], axis=1)
-		col_names.remove(temp)
-		features.append(temp)
-		#print(df)
-	print(df)
-	results  = third_algorithm(df, len(col_names)-1)
-	strong_features = find_strong_feature(results)
-	features.append(col_names[strong_features[0]])
-	print("FINAL RESULTS:", features, strong_features[0])
-	print(df)
-	'''
-
 main()
-'''
-small 35 = 6,3,2
-large 2 = 1,3,24
-'''
 
